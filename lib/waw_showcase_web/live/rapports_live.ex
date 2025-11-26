a10f58a9047b3fef694d79e3fe9bf13b5cfd66a7
Rabarijaona AJ <aj@MacBook-Air-de-Rabarijaona.local>
2025-11-26 10:12:59 +0300

    style(rapports): modification des espacements et colonnes

--- lib/waw_showcase_web/live/rapports_live.ex
+++ lib/waw_showcase_web/live/rapports_live.ex
@@ -27,8 +27,7 @@ defmodule WawShowcaseWeb.RapportsLive do
       %{nom: "Voiture 1", entrees_carburant: 12, cout_carburant: 850.50, cout_maintenance: 320.00},
       %{nom: "Voiture 2", entrees_carburant: 10, cout_carburant: 720.30, cout_maintenance: 280.50},
       %{nom: "Camion 1", entrees_carburant: 15, cout_carburant: 1250.75, cout_maintenance: 450.00},
-      %{nom: "Voiture 3", entrees_carburant: 8, cout_carburant: 580.20, cout_maintenance: 210.00},
-      %{nom: "Camion 2", entrees_carburant: 18, cout_carburant: 1520.40, cout_maintenance: 520.00}
+      %{nom: "Voiture 3", entrees_carburant: 8, cout_carburant: 580.20, cout_maintenance: 210.00}
     ]
   end
 end
