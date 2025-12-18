1956aaf781bc4ca845a3029f2c082566f976c9cd
Rabarijaona AJ <aj@MacBook-Air-de-Rabarijaona.local>
2025-12-18 11:25:30 +0300

    fix: dependance gettext non compatible avec waw

--- mix.exs
+++ mix.exs
@@ -52,7 +52,7 @@ defmodule WawShowcase.MixProject do
       {:req, "~> 0.5"},
       {:telemetry_metrics, "~> 1.0"},
       {:telemetry_poller, "~> 1.0"},
-      {:gettext, "~> 1.0"},
+      {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
       {:bandit, "~> 1.5"},
